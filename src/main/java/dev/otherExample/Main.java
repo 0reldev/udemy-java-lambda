1032f98f972e5f983b936c55bfcdf9b65ab6748a
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-07-05 17:31:51 +0200

    Examples of use of the Predicate interface and the Consumer interface.

--- src/main/java/dev/otherExample/Main.java
+++ src/main/java/dev/otherExample/Main.java
@@ -1,7 +1,10 @@
 package dev.otherExample;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
 
 public class Main {
 
@@ -43,11 +46,63 @@ public class Main {
 
         var result3 = calculator((a, b) -> a.toUpperCase() + " " + b.toUpperCase(), "Ralph", "Kramden");
 //        Result of operation: RALPH KRAMDEN
+
+        var coords = Arrays.asList(
+                new double[]{47.2160, -95.2348},
+                new double[]{29.1566, -89.2495},
+                new double[]{35.1556, -90.0659});
+        coords.forEach(s -> System.out.println(Arrays.toString(s)));
+//          [47.216, -95.2348]
+//          [29.1566, -89.2495]
+//          [35.1556, -90.0659]
+
+        BiConsumer<Double, Double> p1 = (lat, lng) -> System.out.printf("[lat:%.3f lon:%.3f]%n", lat, lng);
+        var firstPoint = coords.get(0);
+        processPoint(firstPoint[0], firstPoint[1], p1);
+//        [lat:47,216 lon:-95,235]
+
+        System.out.println("------");
+        coords.forEach(s -> processPoint(s[0], s[1], p1));
+//          [lat:47,216 lon:-95,235]
+//          [lat:29,157 lon:-89,250]
+//          [lat:35,156 lon:-90,066]
+
+        coords.forEach(s -> processPoint(s[0], s[1], (lat, lng) -> System.out.printf("[lat:%.3f lon:%.3f]%n", lat, lng)));
+//          [lat:47,216 lon:-95,235]
+//          [lat:29,157 lon:-89,250]
+//          [lat:35,156 lon:-90,066]
+
+        list.removeIf(s -> s.equalsIgnoreCase("bravo"));
+        list.forEach(s -> System.out.println(s));
+//        alpha
+//        charlie
+//        delta
+
+        list.addAll(List.of("echo", "easy", "earnest"));
+        list.forEach(s -> System.out.println(s));
+//        alpha
+//        charlie
+//        delta
+//        echo
+//        easy
+//        earnest
+
+        System.out.println("---------");
+        list.removeIf(s -> s.startsWith("ea"));
+        list.forEach(s -> System.out.println(s));
+//        alpha
+//        charlie
+//        delta
+//        echo
     }
 
-    public static <T> T calculator(Operation<T> function, T value1, T value2) {
-        T result = function.operate(value1, value2);
+    public static <T> T calculator(BinaryOperator<T> function, T value1, T value2) {
+        T result = function.apply(value1, value2);
         System.out.println("Result of operation: " + result);
         return result;
+    }
+
+    public static <T> void processPoint(T t1, T t2, BiConsumer<T, T> consumer) {
+        consumer.accept(t1, t2);
     }
 }
